3727ca0e841929da828ac47d00099f370f77bc03
msl8r <matt.slater@hmcts.net>
2022-08-23 15:48:41 +0100

    DTSPO-6556 - remove product var, not needed

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -20,7 +20,7 @@ if [[ ${BUILD} =~ ^pr-.* ]]; then
 elif [[ ${BUILD} == "prod" ]]; then
   TAG="prod-$(git show --no-patch --no-notes --pretty=format:"%h-%ad" --date=format:'%Y%m%d%H%M%S' "${GITHUB_SHA}")"
   echo "Promoting ${ACR_REPO}:pr-${GITHUB_EVENT_NUMBER} to ${ACR_REPO}:${TAG}"
-  az acr import --force -n "${REGISTRY_NAME}" --subscription "${REGISTRY_SUB}" --source "${ACR_REPO}:pr-${GITHUB_EVENT_NUMBER}" -t "${PRODUCT}/${APP_NAME}:${TAG}"
+  az acr import --force -n "${REGISTRY_NAME}" --subscription "${REGISTRY_SUB}" --source "${ACR_REPO}:pr-${GITHUB_EVENT_NUMBER}" -t "${APP_NAME}:${TAG}"
 
 else
   echo "Build type not recognised, use pr-{pr_number} or prod"
